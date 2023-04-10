acfcc8dd83242cffc05609e52476d3cd06cc4e2b
Ryan <weaverryan575@gmail.com>
2023-04-10 16:56:08 -0400

    test

--- slab.c
+++ slab.c
@@ -10,10 +10,42 @@ unsigned char *slab_allocate()
 
   // fill in
 
+  // Find the first available slab with empty slots
+    slab_index = __builtin_ffs(partial_mask) - 1;
+    if (slab_index == -1) {
+        // If no partially filled slabs, use an empty one
+        slab_index = __builtin_ffs(empty_mask) - 1;
+        if (slab_index == -1) return NULL; // No available slabs
+        empty_mask ^= (short)(1 << (15 - slab_index));
+    }
+
+    // Find the first free object within the slab
+    obj_index = __builtin_ffs(~s[slab_index].free_mask) - 1;
+
+    // Update the slab's free mask and free count
+    s[slab_index].free_mask |= (short)(1 << (15 - obj_index));
+    s[slab_index].free_count--;
+
+    // Update the slab's status (partial or full)
+    if (s[slab_index].free_count == 0) {
+        partial_mask ^= (short)(1 << (15 - slab_index));
+        full_mask |= (short)(1 << (15 - slab_index));
+    } else {
+        partial_mask |= (short)(1 << (15 - slab_index));
+    }
+
+    // Calculate the address of the allocated object
+    addr = start + (slab_index << 12) + (obj_index << 8);
+
+
+
+
+  // end
+
   return(addr);
 }
 
-int slab_release(unsigned char *addr)
+int slab_release( unsigned char *addr )
 {
   unsigned int sanity_check = ((int)(addr - start))>>16;  /* should be 0 */
   unsigned int slab_index = ((int)(addr - start))>>12;
